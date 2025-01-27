e82dd49b45174ae53b243af33d70c1bb2b674083
Eric Park <ericparkmusic@gmail.com>
2025-01-26 21:19:10 -0800

    BE: Fix CORS locally

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from Controllers.flickrController import get_photos
 from dotenv import load_dotenv
@@ -6,17 +7,11 @@ from flask_cors import CORS
 load_dotenv()
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:51732"}})
-
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({'message': 'Hello World!'})
+cors = CORS(app, origins=[os.getenv('LOCAL_ORIGIN')])
 
 @app.route('/photos', methods=['GET'])
 def photos():
-    response = jsonify(get_photos())
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(get_photos())
 
 if __name__ == '__main__':
     app.run(debug=True)

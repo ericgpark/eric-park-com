470a9dc4337cff11e5ac0cdd4e847f3ff3c1e8d4
Eric Park <ericparkmusic@gmail.com>
2025-01-28 17:11:09 -0800

    Add server-side caching

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, jsonify
+from flask_caching import Cache
 from Controllers.flickrController import get_photos
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -7,11 +8,27 @@ from flask_cors import CORS
 load_dotenv()
 
 app = Flask(__name__)
+
 cors = CORS(app, origins=[os.getenv('LOCAL_ORIGIN')])
+
+# Set up server side cache to store photo urls
+app.config["SECRET_KEY"] = "any random string"
+app.config["CACHE_TYPE"] = "SimpleCache"
+app.config["CACHE_DEFAULT_TIMEOUT"] = 300 # timeout in seconds  
+cache = Cache(app)
+
 
 @app.route('/photos', methods=['GET'])
 def photos():
-    return jsonify(get_photos())
+    # Check if photos are cached
+    cached_photos = cache.get('photos')
+    if cached_photos:
+        return cached_photos
+
+    # If not, fetch photos from the Flickr API
+    photos = jsonify(get_photos())
+    cache.set('photos', photos)
+    return photos
 
 if __name__ == '__main__':
     app.run(debug=True)

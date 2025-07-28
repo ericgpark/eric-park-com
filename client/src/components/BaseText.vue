f621bacd68b4b4be6934ad83c041574c766de825
ericgpark <ericparkmusic@gmail.com>
2025-07-28 15:47:45 -0700

    Create reusable text component

---
+++ client/src/components/BaseText.vue
@@ -0,0 +1,43 @@
+<template>
+  <component
+    class="base-text"
+    :class="type"
+    :is="tag"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  tag: 'h1' | 'h2' | 'h3' | 'p' | 'span',
+  type: 'heading' | 'subheading' | 'paragraph' | 'caption',
+}>();
+
+</script>
+
+<style scoped>
+
+.base-text {
+  font-family: var(--font-family);
+  color: var(--text-color);
+
+  &.heading {
+    font-size: var(--font-size-heading);
+  }
+
+  &.subheading {
+    font-size: var(--font-size-subheading);
+  }
+
+  &.paragraph {
+    font-size: var(--font-size-paragraph);
+  }
+
+  &.caption {
+    font-size: var(--font-size-caption);
+  }
+}
+
+</style>

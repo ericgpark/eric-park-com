6a1e24b1fd05d26862553ba2d2b86c2b46f5ca75
ericgpark <ericparkmusic@gmail.com>
2025-11-17 20:39:07 -0800

    Create enum

---
+++ client/src/enums/categories.ts
@@ -0,0 +1,60 @@
+import type { Category } from '@/types';
+
+export const categories: Category[] = [
+  {
+    id: 'all',
+    name: 'All'
+  },
+  {
+    id: 'paris',
+    name: 'Paris'
+  },
+  {
+    id: 'taipei',
+    name: 'Taipei'
+  },
+  {
+    id: 'seoul',
+    name: 'Seoul'
+  },
+  {
+    id: 'tokyo',
+    name: 'Tokyo'
+  },
+  {
+    id: 'montreal',
+    name: 'Montreal'
+  },
+  {
+    id: 'los_angeles',
+    name: 'Los Angeles'
+  },
+  {
+    id: 'costa_mesa',
+    name: 'Costa Mesa'
+  },
+  {
+    id: 'hong_kong',
+    name: 'Hong Kong'
+  },
+  {
+    id: 'huntington_beach',
+    name: 'Huntington Beach'
+  },
+  {
+    id: 'denver',
+    name: 'Denver'
+  },
+  {
+    id: 'singapore',
+    name: 'Singapore'
+  },
+  {
+    id: 'hanoi',
+    name: 'Hanoi'
+  },
+  {
+    id: 'misc',
+    name: 'Misc'
+  }
+];
